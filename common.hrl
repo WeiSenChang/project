e8f023cca44422c387b9541015c7295c8ed16cd3
weisenchang <2240974337@qq.com>
2023-06-10 10:54:44 +0800

    init commit

---
+++ common.hrl
@@ -0,0 +1,17 @@
+%% -*- coding: utf-8 -*-
+%%%-------------------------------------------------------------------
+%%% @author weisenchang
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10. 6月 2023 10:35
+%%%-------------------------------------------------------------------
+-author("weisenchang").
+
+-ifndef('common_HRL').
+-define('common_HRL', true).
+
+
+
+-endif.
