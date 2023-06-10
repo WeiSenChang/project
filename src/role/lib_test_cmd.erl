829dbe0c1be64beecc6f9d840d7da1f5c241cc38
weisenchang <2240974337@qq.com>
2023-06-10 11:37:46 +0800

    初步提交

--- src/role/lib_test_cmd.erl
+++ src/role/lib_test_cmd.erl
@@ -7,26 +7,8 @@
 
 %% API
 -export([
-    role_gm/5,
-    reset_role/0
+    role_gm/5
 ]).
-
-reset_role() ->
-    RoleList = lib_role_manage:get_role_list(),
-    reset_role(RoleList, #{}).
-reset_role([], AccountMap) ->
-    {RemoveList, RoleList} = maps:fold(
-        fun(_Account, [KeyValue|T], {AccRemoveList, AccRoleList}) ->
-            {AccRemoveList++T, [KeyValue|AccRoleList]}
-        end, {[], []}, AccountMap),
-    lib_role_manage:put_role_list(RoleList),
-    lists:foreach(fun(KeyValue) -> lib_role_manage:put_account(KeyValue) end, RoleList),
-    lists:foreach(fun(#key_value{key = Id}) -> lib_role_login:logout(Id) end, RemoveList);
-reset_role([KeyValue|T], AccountMap) ->
-    Role = role:key_value_to_role(KeyValue),
-    Account = Role#role.account,
-    KeyValueList = maps:get(Account, AccountMap, []),
-    reset_role(T, maps:put(Account, [KeyValue|KeyValueList], AccountMap)).
 
 role_gm(Gm, _Par1, _Par2, _Par3, _Par4) ->
     ?WARNING("no role gm: ~w", [Gm]).
