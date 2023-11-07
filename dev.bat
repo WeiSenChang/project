c2a73627c6e4e4c46c12f57f76b8f99a191d3483
weisenchang <2240974337@qq.com>
2023-11-07 08:55:46 +0800

    代码修改

--- dev.bat
+++ dev.bat
@@ -57,7 +57,7 @@ goto wait_input
 :attach
     set var=
     set tick=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-    echo %tick%
+    set "tick=%tick: =%"
     start werl -setcookie '%cookie%' -name attach_%tick%_%node% -remsh %node%
     goto wait_input
 
