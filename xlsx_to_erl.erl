e3667b6164cf81d6c950793f9be194c94a9dce6c
weisenchang <2240974337@qq.com>
2023-12-15 18:24:26 +0800

    xlsx_to_erl优化

--- xlsx_to_erl.erl
+++ xlsx_to_erl.erl
@@ -10,6 +10,11 @@
 -define(LOADING, 1).
 -define(LOADED, 2).
 
+
+-record(table, {key_type, type_map = #{}, rows = []}).
+-record(table_field, {col, type, name = ""}).
+
+
 main(_) ->
     case filelib:ensure_dir(?ERL_DIR) of
         ok ->
@@ -82,9 +87,13 @@ spawn_xlsx_to_erl(FileName, Mod) ->
 do_spawn_xlsx_to_erl(FileName, Mod) ->
     case zip:unzip(FileName, [memory]) of
         {ok, FileBin} ->
-            Sheets = xml_to_erl(FileBin),
-            sheets_to_erl(Mod, Sheets),
-            io:format("transform ~ts success~n", [Mod]);
+            case xml_to_erl(FileBin) of
+                {Share, Tables} when length(Tables) > 0 ->
+                    sheets_to_erl(Share, Mod, Tables),
+                    io:format("transform ~ts success~n", [Mod]);
+                _ ->
+                    ignore
+            end;
         _ ->
             ignore
     end.
@@ -94,154 +103,230 @@ xml_to_erl(FileBin) ->
     Share = clean_share(V),
     Amount = 1,
     Tables = xml_to_erl(FileBin, Share, Amount, []),
-    lists:reverse(Tables).
+    {Share, lists:reverse(Tables)}.
 
 xml_to_erl(FileBin, Share, Amount, Tables) ->
     SheetName = "sheet" ++ to_list(Amount),
     SheetPath = "xl/worksheets/" ++ SheetName ++ ".xml",
     case proplists:get_value(SheetPath, FileBin, undefined) of
-        undefined -> Tables;
+        undefined ->
+            Tables;
         SheetXML ->
-            Sheet = clean_sheet(SheetXML),
-            Table = {SheetName, pack_table(Share, Sheet)},
-            xml_to_erl(FileBin, Share, Amount+1, [Table | Tables])
+            NewTables = clean_sheet(Tables, Share, SheetXML),
+            xml_to_erl(FileBin, Share, Amount + 1, NewTables)
     end.
 
-clean_share(undefined) -> [];
+clean_share(undefined) -> #{};
 clean_share(Share) ->
     {SST, _Rest} = xmerl_scan:string(binary_to_list(Share)),
-    [unicode:characters_to_binary(lists:flatten(clean_share_content(SI))) || SI <- SST#xmlElement.content].
+    {_, ShareMap} = lists:foldl(
+        fun(SI, {N, Map}) ->
+            {N + 1, maps:put(N, unicode:characters_to_binary(clean_share_content(SI)), Map)}
+        end, {1, #{}}, SST#xmlElement.content),
+    ShareMap.
 
 clean_share_content([]) -> [];
 clean_share_content(#xmlText{value = Value}) -> Value;
 clean_share_content(#xmlElement{content = Content}) -> clean_share_content(Content);
 clean_share_content(L) -> [clean_share_content(T) || T <- L].
 
-
-clean_sheet(undefined) -> [];
-clean_sheet(Sheet) ->
+clean_sheet(Tables, _Share, undefined) ->
+    Tables;
+clean_sheet(Tables, Share, Sheet) ->
     {Root, _Rest} = xmerl_scan:string(binary_to_list(Sheet)),
     SheetData = lists:keyfind(sheetData, #xmlElement.name, Root#xmlElement.content),
-    [clean_sheet_row(Row) || Row <- SheetData#xmlElement.content, Row#xmlElement.name == row].
+    case SheetData#xmlElement.content of
+        [_,FieldRow,TypeRow,_,_|Rows] when length(Rows) > 0 ->
+            case get_field_info(FieldRow, TypeRow, Share) of
+                {KeyType, TypeMap} when map_size(TypeMap) > 0 ->
+                    [#table{key_type = KeyType, type_map = TypeMap, rows = Rows} | Tables];
+                _ ->
+                    Tables
+            end;
+        _ ->
+            Tables
+    end.
 
-clean_sheet_row(Row) ->
-    [clean_sheet_c(C) || C <- Row#xmlElement.content, C#xmlElement.name == c].
+get_field_info(FieldRow, TypeRow, Share) ->
+    FieldMap = clean_sheet_row(Share, FieldRow),
+    TypeMap = clean_sheet_row(Share, TypeRow),
+    NewTypeMap = to_type_map(TypeMap),
+    NewFieldMap = to_field_map(FieldMap, NewTypeMap),
+    case lists:sort(maps:keys(NewFieldMap)) of
+        [KeyCol|FieldCols] when length(FieldCols) > 0 ->
+            {maps:get(KeyCol, NewFieldMap), maps:remove(KeyCol, NewFieldMap)};
+        _ ->
+            {#table_field{}, #{}}
+    end.
 
-clean_sheet_c(C) ->
+to_type_map(TypeMap) ->
+    maps:fold(
+        fun(Col, Type, Acc) ->
+            case Type of
+                <<"int">> ->
+                    maps:put(Col, int, Acc);
+                <<"long">> ->
+                    maps:put(Col, int, Acc);
+                <<"float">> ->
+                    maps:put(Col, float, Acc);
+                <<"string">> ->
+                    maps:put(Col, string, Acc);
+                <<"json">> ->
+                    maps:put(Col, list, Acc);
+                _ ->
+                    Acc
+            end
+        end, #{}, TypeMap).
+
+to_field_map(FieldMap, TypeMap) ->
+    maps:fold(
+        fun(Col, Type, Acc) ->
+            Field = maps:get(Col, FieldMap),
+            Name = to_list(Field),
+            TableField = #table_field{col = Col, type = Type, name = Name},
+            maps:put(Col, TableField, Acc)
+        end, #{}, TypeMap).
+
+clean_sheet_row(Share, Row) ->
+    lists:foldl(
+        fun(C, Acc) ->
+            {Col, V} = clean_sheet_c(Share, C),
+            maps:put(Col, V, Acc)
+        end, #{}, Row#xmlElement.content).
+
+clean_sheet_c(Share, C) ->
     T = lists:keyfind(t, #xmlAttribute.name, C#xmlElement.attributes),
     V = lists:keyfind(v, #xmlElement.name,   C#xmlElement.content),
     #xmlAttribute{value = Pos} = lists:keyfind(r, #xmlAttribute.name, C#xmlElement.attributes),
-    clean_sheet_v(col(Pos), T, V).
+    clean_sheet_v(Share, col(Pos), T, V).
 
-%% 表格没有内容，为 null
-clean_sheet_v(Col, #xmlAttribute{value = "s"}, #xmlElement{content = [R]}) ->
-    {transform, Col, list_to_integer(R#xmlText.value)};
-clean_sheet_v(Col, _, #xmlElement{content = [R]}) ->
+clean_sheet_v(Share, Col, #xmlAttribute{value = "s"}, #xmlElement{content = [R]}) ->
+    N = list_to_integer(R#xmlText.value),
+    {Col, maps:get(N + 1, Share)};
+clean_sheet_v(_Share, Col, _, #xmlElement{content = [R]}) ->
     {Col, R#xmlText.value};
-clean_sheet_v(Col, _V1, _V2) ->
+clean_sheet_v(_Share, Col, _V1, _V2) ->
     {Col, null}.
 
 col(Pos) ->
-    [V || V <- Pos, V >= $A andalso V =< $Z].
+    list_to_atom([V || V <- Pos, V >= $A andalso V =< $Z]).
 
-pack_table(Share, Sheet) ->
-    [pack_row(Row, Share) || Row <- Sheet].
-
-pack_row(Row, Share) ->
-    [pack_value(V, Share) || V <- Row].
-
-pack_value({transform, Col, V}, Share) -> {Col, lists:nth(V+1, Share)};
-pack_value(V, _Share) -> V.
-
-
-sheets_to_erl(Name, Sheets) ->
-    SheetFields = [get_record_fields(Sheet) || {_, Sheet} <- Sheets],
+sheets_to_erl(Share, Name, Tables) ->
     Mod = "cfg_" ++ Name,
+    FileName = ?ERL_DIR ++ Mod ++ ".erl",
+    {ok, Fd} = file:open(FileName, [write]),
     ErlHeader = gen_erl_header(Mod),
-    ErlBody = gen_erl_body(SheetFields, Sheets),
-    Str = ErlHeader ++ ErlBody,
-    file:write_file(?ERL_DIR ++ Mod ++ ".erl", unicode:characters_to_binary(Str, utf8)),
+    file:write(Fd, unicode:characters_to_binary(ErlHeader, utf8)),
+    KeyStrList = sheets_to_erl(Fd, Share, [], Tables),
+    Str = "get(_) ->\r\n\tundefined.\r\n
+    get_list() ->\r\n\t[" ++ string:join(KeyStrList, ",") ++ "].",
+    file:write(Fd, unicode:characters_to_binary(Str, utf8)),
+    file:close(Fd),
     ok.
 
-gen_erl_body(SheetFields, Sheets) ->
-    {Str0, StrList} = gen_erl_body("", [], SheetFields, Sheets),
-    Str1 = "get(_) ->\r\n\tundefined.\r\n\r\n",
-    Str2 = "get_list() ->\r\n\t[" ++ string:join(StrList, ",") ++ "].",
-    Str0 ++ Str1 ++ Str2.
-gen_erl_body(Str0, StrList, [], []) ->
-    {Str0, lists:reverse(StrList)};
-gen_erl_body(Str0, StrList, [{{KeyCol, _, Type}, Fields} | Tail0], [{_, [_,_,_,_,_|Rows]} | Tail1]) ->
-    case length(Fields) > 0 of
-        true ->
-            {NewStr0, NewStrList} = gen_erl_body(KeyCol, Type, Str0, StrList, Fields, Rows),
-            gen_erl_body(NewStr0, NewStrList, Tail0, Tail1);
-        false ->
-            gen_erl_body(Str0, StrList, Tail0, Tail1)
-    end;
-gen_erl_body(Str0, StrList, [_ | Tail0], [_ | Tail1]) ->
-    gen_erl_body(Str0, StrList, Tail0, Tail1).
+gen_erl_header(RecordName) ->
+    "%% -*- coding: utf-8 -*-
+-module(" ++ RecordName ++ ").\r\n
+-export([get/1,get_list/0]).\r\n\r\n".
 
-gen_erl_body(_KeyCol, _Type, Str0, StrList, _, []) ->
-    {Str0, StrList};
-gen_erl_body(KeyCol, Type, Str0, StrList, Fields, [Row | Tail]) ->
-    case lists:keyfind(KeyCol, 1, Row) of
-        {KeyCol, Key} when Key =/= null ->
-            KeyStr = to_str(Type, Key),
-            NewStrList = [KeyStr | StrList],
+sheets_to_erl(_Fd, _Share, KeyStrList, []) ->
+    lists:reverse(KeyStrList);
+sheets_to_erl(Fd, Share, KeyStrList, [Table | Tail]) ->
+    #table{key_type = #table_field{col = KeyCol, type = KeyType}, type_map = TypeMap, rows = Rows} = Table,
+    NewKeyStrList = sheets_to_erl(Fd, Share, KeyCol, KeyType, KeyStrList, TypeMap, Rows),
+    sheets_to_erl(Fd, Share, NewKeyStrList, Tail).
+
+sheets_to_erl(_Fd, _Share, _KeyCol, _Type, KeyStrList, _, []) ->
+    KeyStrList;
+sheets_to_erl(Fd, Share, KeyCol, KeyType, KeyStrList, TypeMap, [Row | Tail]) ->
+    RowDataMap = clean_sheet_row(Share, Row),
+    case maps:get(KeyCol, RowDataMap, null) of
+        null ->
+            sheets_to_erl(Fd, Share, KeyCol, KeyType, KeyStrList, TypeMap, Tail);
+        Key ->
+            KeyStr = to_key_str(KeyType, Key),
             HeaderStr = "get(" ++ KeyStr ++ ") -> #{\r\n\t",
-            BodyStrList = gen_record_value_str(Fields, Row),
+            BodyStrList = gen_record_value_str(TypeMap, RowDataMap),
             TailStr = "\r\n};\r\n",
             BodyStr = string:join(BodyStrList, ",\r\n\t"),
-            Str1 = HeaderStr ++ BodyStr ++ TailStr,
-            gen_erl_body(KeyCol, Type, Str0 ++ Str1, NewStrList, Fields, Tail);
-        _ ->
-            gen_erl_body(KeyCol, Type, Str0, StrList, Fields, Tail)
+            Str = HeaderStr ++ BodyStr ++ TailStr,
+            file:write(Fd, unicode:characters_to_binary(Str, utf8)),
+            sheets_to_erl(Fd, Share, KeyCol, KeyType, [KeyStr | KeyStrList], TypeMap, Tail)
     end.
 
-gen_record_value_str(Fields, Row) ->
-    gen_record_value_str([], Row, Fields).
-gen_record_value_str(StrList, _Row, []) ->
-    lists:reverse(StrList);
-gen_record_value_str(StrList, Row, [{Col, Name, Type} | Tail]) ->
-    case lists:keyfind(Col, 1, Row) of
-        {Col, Value} when Value =/= null ->
-            Str = to_str(Type, Value),
-            NewStr = to_number_str(Type, Str),
-            NewStrList = [Name ++ " => " ++ NewStr | StrList],
-            gen_record_value_str(NewStrList, Row, Tail);
-        _ ->
-            gen_record_value_str(StrList, Row, Tail)
-    end.
+gen_record_value_str(TypeMap, RowDataMap) ->
+    maps:fold(
+        fun(_, #table_field{col = Col, name = Name, type = Type}, Acc) ->
+            case maps:get(Col, RowDataMap, null) of
+                null ->
+                    Acc;
+                Value ->
+                    Str = to_str(Type, Value),
+                    NewStr = to_number_str(Type, Str),
+                    [Name ++ " => " ++ NewStr | Acc]
+            end
+        end, [], TypeMap).
 
-to_str("string", Value) ->
+
+%%%%%%%%%%%%
+to_list(Value) when is_list(Value) ->
+    Value;
+to_list(Value) when is_binary(Value) ->
+    erlang:binary_to_list(Value);
+to_list(Value) when is_integer(Value) ->
+    erlang:integer_to_list(Value);
+to_list(Value) when is_float(Value) ->
+    erlang:float_to_list(Value);
+to_list(Value) when is_atom(Value) ->
+    erlang:atom_to_list(Value);
+to_list(Value) ->
+    erlang:binary_to_list(erlang:term_to_binary(Value)).
+
+
+to_key_str(string, Value) when is_binary(Value) ->
+    io_lib:format("~ts", [Value]);
+to_key_str(_, Value) ->
+    to_list(Value).
+
+to_str(string, Value) ->
     case is_binary(Value) of
         true ->
-            "\"" ++ io_lib:format("~ts", [Value]) ++ "\"";
+            "<<\"" ++ io_lib:format("~ts", [Value]) ++ "\"/utf8>>";
         false ->
-            "\"" ++ to_list(Value) ++ "\""
+            "<<\"" ++ to_list(Value) ++ "\"/utf8>>"
     end;
-to_str("list", Value) ->
+to_str(list, Value) ->
     io_lib:format("~ts", [Value]);
 to_str(_, Value) ->
     to_list(Value).
 
-to_number_str("int", Str) when length(Str) =< 0 ->
+to_number_str(int, Str) when length(Str) =< 0 ->
     "0";
-to_number_str("int", Str) ->
+to_number_str(int, Str) ->
     case string:tokens(Str, "E") of
         [V1, V2] ->
             to_num_str(V1, V2);
         _ ->
-            Str
+            case lists:member($., Str) of
+                true ->
+                    integer_to_list(round(list_to_float(Str)));
+                false ->
+                    Str
+            end
     end;
-to_number_str("float", Str) when length(Str) =< 0 ->
+to_number_str(float, Str) when length(Str) =< 0 ->
     "0.0";
-to_number_str("float", Str) ->
+to_number_str(float, Str) ->
     case string:tokens(Str, "E") of
         [V1, V2] ->
             to_num_str(V1, V2);
         _ ->
-            Str
+            case lists:member($., Str) of
+                false ->
+                    Str ++ ".0";
+                true ->
+                    Str
+            end
     end;
 to_number_str(_, Str) ->
     Str.
@@ -285,55 +370,6 @@ to_num_str_by_left([C1|T1], V2, [_C2|T2]) ->
 to_num_str_by_left([], V2, [C2|T2]) ->
     to_num_str_by_left([], [C2-48|V2], T2).
 
-
-gen_erl_header(RecordName) ->
-    "%% -*- coding: utf-8 -*-
--module(" ++ RecordName ++ ").\r\n
--export([get/1,get_list/0]).\r\n\r\n".
-
-get_record_fields([_, Fields, Types | _Tail]) ->
-    NewTypes = get_record_types([], Types),
-    get_record_fields([], Fields, NewTypes);
-get_record_fields(_) ->
-    {{"inf", "undefined", "null"}, []}.
-
-get_record_fields([], _Fields, []) ->
-    {{"inf", "undefined", "null"}, []};
-get_record_fields([KeyField | SheetField], _Fields, []) ->
-    {KeyField, SheetField};
-get_record_fields(SheetField, Fields, [{Col, Type} | Tail]) ->
-    {Col, Filed} = lists:keyfind(Col, 1, Fields),
-    NewFiled = to_list(Filed),
-    NewSheetField = [{Col, NewFiled, Type} | SheetField],
-    get_record_fields(NewSheetField, Fields, Tail).
-
-get_record_types(Types, []) ->
-    Types;
-get_record_types(Types, [{Col, <<"int">>} | Tail]) ->
-    get_record_types([{Col, "int"} | Types], Tail);
-get_record_types(Types, [{Col, <<"long">>} | Tail]) ->
-    get_record_types([{Col, "int"} | Types], Tail);
-get_record_types(Types, [{Col, <<"float">>} | Tail]) ->
-    get_record_types([{Col, "float"} | Types], Tail);
-get_record_types(Types, [{Col, <<"string">>} | Tail]) ->
-    get_record_types([{Col, "string"} | Types], Tail);
-get_record_types(Types, [{Col, <<"json">>} | Tail]) ->
-    get_record_types([{Col, "list"} | Types], Tail);
-get_record_types(Types, [_ | Tail]) ->
-    get_record_types(Types, Tail).
-
-to_list(Value) when is_list(Value) ->
-    Value;
-to_list(Value) when is_binary(Value) ->
-    erlang:binary_to_list(Value);
-to_list(Value) when is_integer(Value) ->
-    erlang:integer_to_list(Value);
-to_list(Value) when is_float(Value) ->
-    erlang:float_to_list(Value);
-to_list(Value) when is_atom(Value) ->
-    erlang:atom_to_list(Value);
-to_list(Value) ->
-    erlang:binary_to_list(erlang:term_to_binary(Value)).
 
 %%%%%%%%
 set_mod_state(Mod, State) ->
